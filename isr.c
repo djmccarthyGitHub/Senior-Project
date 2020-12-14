b721b26f51fe478189055fec3ac0b0ef547f92a8
duuufe <70453662+duuufe@users.noreply.github.com>
2020-12-13 17:20:48 -0800

    Update and rename isr2.c to isr.c

--- isr.c
+++ isr.c
@@ -1,3 +1,13 @@
+/*
+ * This file contains our main function which
+ * initializes all gpio and the ISRs for our motion
+ * sensor, push button, and killswitch. 
+ *
+ * The program gnuplot is used to plot the 
+ * acceleration vs time and position vs time 
+ * values from our freq_peak and accel_peak arrays.
+ */
+
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
@@ -13,15 +23,15 @@
 #define MAX_RUNTIME 60 //60 seconds max runtime
 #define MAX_FFTS 6000 //MAX_RUNTIME * FS / NOS
 
-int start = 0;
-int start_ks = 0;
+int start = 0; //compared to motionSensor tick value (used for debounce)
+int start_ks = 0; //compared to killSwitch tick value (used for debounce)
 int last_level = 1;
-int count;
-int flag;
-int botCB, topOOL;
-int opened = 0;
-int repeat;
-float cbs_per_reading;
+int count; //Number of FFTS taken
+int flag; //Indicates criteria has been met for dispension halt
+int botCB, topOOL; //values used for ADC
+int opened = 0; //Indicates files stored into FP
+int repeat; //Number of repeat freq_peak values
+float cbs_per_reading; //used for ADC
 
 float freq_peak[MAX_FFTS];
 float accel_peak[MAX_FFTS];
@@ -29,19 +39,18 @@ float t[MAX_FFTS];
 float start_freq, end_freq;
 float thres_passed;
 
-FILE *fp1, *fp2;
-FILE *plot_accel, *plot_pos;
+FILE *fp1, *fp2; //Stores Accel v Time and Pos v Time in file
+FILE *plot_accel, *plot_pos; //Used to pull up plots for AvT and PvT
 
 void killSwitch(int gpio, int level, uint32_t tick);
 
 void motionSensor(int gpio, int level, uint32_t tick)
 {
-    //printf("%d\n", tick);
     if (tick - start > 250000) {
-	
+	//Check for level (active low)
 	if (!level && last_level != level && gpioRead(17)) {
 	printf("Motion sensor activated\n");
-	//printf("cbs_per_reading in isr: %f %d %d  \n", cbs_per_reading, botCB, topOOL);
+	//Open files for plotting
 	fp1 = fopen("PvT.txt", "wb");
 	fp2 = fopen("AvT.txt", "wb");
 	plot_accel = popen("gnuplot -persist", "w");
@@ -66,8 +75,11 @@ void motionSensor(int gpio, int level, uint32_t tick)
 	adc(botCB, topOOL, &cbs_per_reading);
 	printf("ADC complete\n");
 	
+	// Momentarily disable killswitch to write FFT values
+	// to files and display graphs 
 	gpioSetISRFunc(17, FALLING_EDGE, 0, NULL);
 	
+	//Store and reset freq_peak and accel_peak values	
 	for (int j = 0; j < count; j++) {
 		fprintf(fp1, "%f\t%f\n", t[j], freq_peak[j]);
 		fprintf(fp2, "%f\t%f\n", t[j], accel_peak[j]);
@@ -77,6 +89,8 @@ void motionSensor(int gpio, int level, uint32_t tick)
 	
 	fclose(fp1);
 	fclose(fp2);
+	
+	//Plot Acceleration vs Time and Position vs Time
 	fprintf(plot_accel, "set term wxt title 'Acceleration vs Time'\n");
 	fprintf(plot_accel, "set xlabel \"Time (s)\"\n");
 	fprintf(plot_accel, "set ylabel \"Acceleration (Hz/s^2)\"\n");
@@ -89,12 +103,22 @@ void motionSensor(int gpio, int level, uint32_t tick)
 	fclose(plot_pos);
 	//printf("Start freq: %f\t End freq: %f\n", start_freq, end_freq);
 	//printf("Time crossed: %f\n", thres_passed);
+	
+	//Indicate files have been closed to other processes
 	opened = 0;
+	
+	//Used as another debounce measure
+	//When sensor goes high we do not want it to 
+	//trigger dispense if it bounces low
 	last_level = level;
+	//Reenable killswitch ISR
 	gpioSetISRFunc(17, FALLING_EDGE, 0, killSwitch);
 	time_sleep(0.1);
 	}
 	
+	// If statement determines when dispension should halt
+	// (when criteria from FFT algorithm has been met or
+	// bottle has been pulled away from IR sensor)
 	if (gpioRead(25) || flag) {
 		if (flag) {
 			printf("Fill completed\n");
@@ -108,7 +132,6 @@ void motionSensor(int gpio, int level, uint32_t tick)
 		time_sleep(0.100);
 		gpioWrite(16, 0); //Turn off pump
 		gpioWrite(3, 0);  // turn off LED
-		// save data to file
 		last_level = level;
 	}
     }
@@ -161,6 +184,7 @@ void killSwitch(int gpio, int level, uint32_t tick)
 				}	
 				fclose(fp1);
 				fclose(fp2);
+				//Plot values taken from FFTs
 				fprintf(plot_accel, "set term wxt title 'Acceleration vs Time'\n");
 				fprintf(plot_accel, "set xlabel \"Time (s)\"\n");
 				fprintf(plot_accel, "set ylabel \"Acceleration (Hz/s^2)\"\n");
@@ -198,21 +222,22 @@ void killSwitch(int gpio, int level, uint32_t tick)
 
 int main () {
    	if (gpioInitialise() < 0) return 1;
-	gpioSetMode(16, PI_OUTPUT);
-	gpioSetMode(6, PI_OUTPUT);
-	gpioSetMode(25, PI_INPUT);
-	gpioSetMode(3, PI_OUTPUT);
-	gpioSetMode(17, PI_INPUT);
-	gpioSetMode(23, PI_INPUT);
+	gpioSetMode(16, PI_OUTPUT); //Pump
+	gpioSetMode(6, PI_OUTPUT); //Valve
+	gpioSetMode(25, PI_INPUT); //Motion sensor
+	gpioSetMode(3, PI_OUTPUT); //LED
+	gpioSetMode(17, PI_INPUT); //Killswitch
+	gpioSetMode(23, PI_INPUT); //Push button
+	//Initialize all pins used for ADC
 	adc_ini(&botCB, &topOOL, &cbs_per_reading); 
 
-	gpioSetISRFunc(25, EITHER_EDGE, 0, motionSensor);
-	gpioSetISRFunc(17, FALLING_EDGE, 0, killSwitch);
-	gpioSetISRFunc(23, EITHER_EDGE, 0, manualButton);
+	gpioSetISRFunc(25, EITHER_EDGE, 0, motionSensor); //Set motion sensor ISR
+	gpioSetISRFunc(17, FALLING_EDGE, 0, killSwitch); //Set killswitch ISR
+	gpioSetISRFunc(23, EITHER_EDGE, 0, manualButton); //Push button ISR
 
-	gpioWrite(3, 0);
-	gpioWrite(6, 0);
-	gpioWrite(16, 0);
+	gpioWrite(3, 0); //Set LED low
+	gpioWrite(6, 0); //Set valve low
+	gpioWrite(16, 0); //Set pump low
 
-	while (1);
+	while (1); //Continue running
 }
